ec6fe5c4aedfe5be5dfdab7f81f7a089b0e22462
PrPleGoo <felix.leeuwen@gmail.com>
2014-10-29 12:03:51 +0100

    added HeadLamp
    Test commit

---
+++ Source/CubeLand/HeadLamp.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CubeLand.h"
+#include "HeadLamp.h"
+
+
+AHeadLamp::AHeadLamp(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/CubeLand/HeadLamp.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "HeadLamp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBELAND_API AHeadLamp : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
